1769c30465509e1b6eea989e56acc72a5b55ceaa
Josepogers <zekreds12@gmail.com>
2025-03-02 12:40:34 -0300

    Tentativa de estilizar a tela de home

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:clinica_hd/recepcao_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,7 +8,173 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('Clinica HD'),
+        backgroundColor: Color(0xFF145388),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Primeira linha de botões
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RecepcaoPage(),
+                  ),
+                ),
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blueGrey[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.home,
+                        color: Colors.black,
+                      ),
+                      Text("Recepção", style: TextStyle(color: Colors.black)),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(width: 20), // Espaçamento entre os botões
+              InkWell(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RecepcaoPage(),
+                  ),
+                ),
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blueGrey[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.home,
+                        color: Colors.black,
+                      ),
+                      Text("Técnica", style: TextStyle(color: Colors.black)),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(width: 20), // Espaçamento entre os botões
+              InkWell(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RecepcaoPage(),
+                  ),
+                ),
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blueGrey[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.home,
+                        color: Colors.black,
+                      ),
+                      Text("Administração",
+                          style: TextStyle(color: Colors.black)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // Espaçamento entre as linhas
+          SizedBox(height: 20),
+
+          // Segunda linha de botões
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RecepcaoPage(),
+                  ),
+                ),
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blueGrey[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.home,
+                        color: Colors.black,
+                      ),
+                      Text("Laudos", style: TextStyle(color: Colors.black)),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(width: 20), // Espaçamento entre os botões
+              InkWell(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RecepcaoPage(),
+                  ),
+                ),
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blueGrey[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.home,
+                        color: Colors.black,
+                      ),
+                      Text("Gerência", style: TextStyle(color: Colors.black)),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(width: 20), // Espaçamento entre os botões
+              InkWell(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RecepcaoPage(),
+                  ),
+                ),
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.blueGrey[100],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.home,
+                        color: Colors.black,
+                      ),
+                      Text("Faturamento",
+                          style: TextStyle(color: Colors.black)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
