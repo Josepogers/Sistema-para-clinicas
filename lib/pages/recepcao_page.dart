060a0efc96ad7349c0803e36fd485e39e0cecf06
Josepogers <zekreds12@gmail.com>
2025-03-05 19:47:51 -0300

    comeco da app bar

---
+++ lib/pages/recepcao_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class RecepcaoPage extends StatefulWidget {
+  const RecepcaoPage({super.key});
+
+  @override
+  State<RecepcaoPage> createState() => _RecepcaoPageState();
+}
+
+class _RecepcaoPageState extends State<RecepcaoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Recepcao'),
+        centerTitle: true,
+        actions: [
+          Tooltip(
+            message: 'Adicionar novo Paciente',
+            child: Icon(Icons.add),
+          ),
+          Tooltip(
+            message: 'Adicionar novo Paciente',
+            child: Icon(Icons.add),
+          ),
+          Tooltip(
+            message: 'Adicionar novo Paciente',
+            child: Icon(Icons.add),
+          )
+        ],
+      ),
+    );
+  }
+}
